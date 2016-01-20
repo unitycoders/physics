eda5cb4a47482bdf83e6d5513895731743adfee4
Joseph Walton-Rivers <jwalto@essex.ac.uk>
2016-01-20 12:06:43 +0000

    vector interface

---
+++ src/main/java/com/fossgalaxy/utils/Vector.java
@@ -0,0 +1,32 @@
+package com.fossgalaxy.utils;
+
+public interface Vector {
+	
+	public int getDimensions();
+	
+	public Vector multiply(Vector other);
+	public Vector multiply(double other);
+	
+	public double dot(Vector other);
+	
+	public Vector add(Vector other);
+	public Vector add(Vector other, double factor);
+
+	public Vector subtract(Vector other);
+	public Vector subtract(Vector other, double factor);
+	
+	public Vector wrap(double width, double height);
+	
+	public Vector normalise();
+	public Vector limit(double limit);
+	
+	public Vector rotate(double rads);
+	public Vector rotateDeg(double deg);
+	
+	public double magnatude();
+	public double distance(Vector other);
+	
+	public double getComponent(int i);
+	
+	public Vector clone();
+}
